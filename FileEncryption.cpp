b5e063154f2f55010413525badf08f4bcdaac54a
deekshita <deekshita.p18@gmail.com>
2022-05-12 19:34:59 +0200

    encryption with RSA

--- FileEncryption.cpp
+++ FileEncryption.cpp
@@ -1,34 +1,56 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<algorithm>
+#include<math.h>
 
 using namespace std;
 
 
 int main(){
 
-    fstream inputFile,outputFile;
+    system("CLS");
+    fstream inputFile,encodedFile;
 
-    inputFile.open("data.txt",ios::in | ios::out);
+    inputFile.open("data.txt",ios::in);
 
-    outputFile.open("encoded.txt",ios::in | ios::out);
+    encodedFile.open("encoded.txt",ios::in | ios::out);
 
+    int p = 47 , q = 53;
+    int n = p*q;
+    int t = (p-1)*(q-1);
+
+    int e =0 , d=0;
+
+    for(int i=2; i<t; i++){
+        if ( __gcd(i,t) == 1 && __gcd(i,n)==1){
+            e = i;            
+        }
+        if(e>100){
+            break;
+        }
+        
+    }
+
+    // File encryption 
     if(inputFile.is_open()){
         string line;
-        char ch;
+         char ch;
+         
          while(inputFile.good()){  //good bit , eos set or not 
         
         getline(inputFile,line);
-
+    
         for(int i=0 ; i<line.length() ; i++){
-                ch=line[i];
-                outputFile<<int(ch);
+                ch= line[i];
+                long double x = pow(int(ch),e);
+                long double enc = fmod(x,n);
+                encodedFile<<enc;
         }
-        outputFile<<endl;
-        
+        encodedFile<<endl;
         }
     inputFile.close(); 
-    outputFile.close();
+    encodedFile.close();
 
     }       
 
